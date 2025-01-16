991d254b2b68859ff183112a6de731c472568843
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2025-01-16 09:14:23 -0300

    feat: add findByNearby and searchMany methods to GymsRepository
    - findByNearby: Retrieve gyms near a specific location.
    - searchMany: Perform paginated gym searches.

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,6 +1,14 @@
 import { Gym } from "@prisma/client";
+import { number } from "zod";
+
+export interface FindManyNearbyParams{
+    latitude: number,
+    longitude: number
+}
 
 export interface GymRepository{
     findById(id: string): Promise<Gym> | null
+    findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
     create(data: Prisma.GymCreateInput): Promise<Gym>
+    searchMany(query: string, page: number): Promise<Gym[]>
 }
