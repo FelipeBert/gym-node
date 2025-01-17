008bae4aee64fbd43eff863082a12e992bb883c5
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2025-01-17 13:29:45 -0300

    feat: add route for ProfileController in routes.ts
    - Added a new route in routes.ts to handle requests to the ProfileController.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import { register } from "./controllers/registerController";
 import { authenticate } from "./controllers/authenticateController";
+import { profile } from "./controllers/profileController";
 
 export async function appRoutes(app: FastifyInstance){
     app.post('/users', register)
     app.post('/sessions', authenticate)
+    app.get('/me', profile)
 }
